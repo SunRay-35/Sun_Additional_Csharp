f8f36acf5063f9fde36b7e816dd9f49c350c5ca7
SunRay-35 <siaip@mail.ru>
2022-11-24 11:06:34 +0300

    task4 рекурсия Жени

---
+++ Snake/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Threading;
+
+class GameThreads
+{
+    public string snakeDirection;
+    public int gameWidth;
+    public int gameHeight;
+    public int offsetTop;
+    public int offsetInfo;
+    public void SnakeActions() // Действия змейки
+    {
+        int delayDuration = 500;
+        double incTail = 0.00000000000001;
+        double[,] gameField = new double[gameHeight, gameWidth];
+        for (int j = 0; j < gameWidth; j++)
+        {
+            gameField[offsetTop, j] = 1;
+            gameField[gameHeight - 1, j] = 1;
+        }
+        for (int i = 1; i < gameHeight - 1; i++)
+        {
+            gameField[i, 0] = 1;
+            gameField[i, gameWidth - 1] = 1;
+        }
+        bool IsAlive = true;
+        int headHeight = Convert.ToInt32(gameHeight / 2);
+        int headWidth = Convert.ToInt32(gameWidth / 2);
+        gameField[headHeight, headWidth] = 1;
+        int tailHeight = headHeight - 1;
+        int tailWidth = headWidth;
+        double tailAge = 2.0;
+        gameField[tailHeight, tailWidth] = tailAge;
+        // System.Console.SetCursorPosition(headWidth, headHeight);
+        // System.Console.Write("*");
+        // System.Console.SetCursorPosition(tailWidth, tailHeight);
+        // System.Console.Write("*");
+        while (IsAlive)
+        {
+            gameField[headHeight, headWidth] = 3;
+            if (snakeDirection == "up" && (gameField[headHeight - 1, headWidth] != 1))
+            {
+                gameField[headHeight - 1, headWidth] = 2;
+                headHeight--;
+                System.Console.SetCursorPosition(headHeight, headWidth);
+                System.Console.Write("*");
+                System.Console.SetCursorPosition(tailWidth, tailHeight);
+                System.Console.Write(" ");
+                gameField[tailHeight, tailWidth] = 0;
+            }
+            else if (snakeDirection == "down" && (gameField[headHeight + 1, headWidth] != 1))
+            {
+                gameField[headHeight + 1, headWidth] = 2;
+                headHeight++;
+                System.Console.SetCursorPosition(headHeight, headWidth);
+                System.Console.Write("*");
+                System.Console.SetCursorPosition(tailWidth, tailHeight);
+                System.Console.Write(" ");
+                gameField[tailHeight, tailWidth] = 0;
+            }
+            else if (snakeDirection == "right" && (gameField[headHeight, headWidth + 1] != 1))
+            {
+                gameField[headHeight, headWidth + 1] = 2;
+                headWidth++;
+                System.Console.SetCursorPosition(headHeight, headWidth);
+                System.Console.Write("*");
+                System.Console.SetCursorPosition(tailWidth, tailHeight);
+                System.Console.Write(" ");
+                gameField[tailHeight, tailWidth] = 0;
+            }
+            else if (snakeDirection == "left" && (gameField[headHeight, headWidth - 1] != 1))
+            {
+                gameField[headHeight, headWidth - 1] = 2;
+                headWidth--;
+                System.Console.SetCursorPosition(headHeight, headWidth);
+                System.Console.Write("*");
+                System.Console.SetCursorPosition(tailWidth, tailHeight);
+                System.Console.Write(" ");
+                gameField[tailHeight, tailWidth] = 0;
+            }
+            else IsAlive = false;
+            if (gameField[tailHeight - 1, tailWidth] == tailAge) tailHeight--;
+            else if (gameField[tailHeight + 1, tailWidth] == tailAge) tailHeight++;
+            else if (gameField[tailHeight, tailWidth - 1] == tailAge) tailWidth--;
+            else if (gameField[tailHeight - 1, tailWidth + 1] == tailAge) tailWidth++;
+            tailAge += incTail;
+            Thread.Sleep(delayDuration);
+        }
+    }
+
+    public void RefreshInterface() // Прорисовка интерфейса игры
+    {
+        int delayDuration = 1000;
+        System.Console.Clear();
+        System.Console.BackgroundColor = System.ConsoleColor.Black;
+        for (int j = 0; j < gameWidth; j++)
+        {
+            System.Console.SetCursorPosition(j, 0);
+            System.Console.Write("═");
+            System.Console.SetCursorPosition(j, offsetTop);
+            System.Console.Write("═");
+            System.Console.SetCursorPosition(j, gameHeight - 1);
+            System.Console.Write("═");
+        }
+        for (int i = 1; i < gameHeight - 1; i++)
+        {
+            System.Console.SetCursorPosition(0, i);
+            System.Console.Write("║");
+            System.Console.SetCursorPosition(gameWidth - 1, i);
+            System.Console.Write("║");
+        }
+        System.Console.SetCursorPosition(0, 0);
+        System.Console.Write("╔");
+        System.Console.SetCursorPosition(gameWidth - 1, 0);
+        System.Console.Write("╗");
+        System.Console.SetCursorPosition(0, offsetTop);
+        System.Console.Write("╠");
+        System.Console.SetCursorPosition(gameWidth - 1, offsetTop);
+        System.Console.Write("╣");
+        System.Console.SetCursorPosition(0, gameHeight - 1);
+        System.Console.Write("╚");
+        System.Console.SetCursorPosition(gameWidth - 1, gameHeight - 1);
+        System.Console.Write("╝");
+        Thread.Sleep(delayDuration);
+    }
+}
+class Application
+{
+    static void Main() // Основное тело программы
+    {
+
+        GameThreads gameProcess = new GameThreads();
+        gameProcess.snakeDirection = "up";
+        gameProcess.gameWidth = System.Console.WindowHeight * 2;
+        gameProcess.gameHeight = System.Console.WindowHeight;
+        gameProcess.offsetTop = 4;
+        gameProcess.offsetInfo = 2;
+        Thread interfaceThread = new Thread(new ThreadStart(gameProcess.RefreshInterface));
+        interfaceThread.IsBackground = true;
+        interfaceThread.Start();
+        Thread snakeThread = new Thread(new ThreadStart(gameProcess.SnakeActions));
+        snakeThread.IsBackground = true;
+        snakeThread.Start();
+        bool isActive = true;
+        while (isActive)
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Escape) isActive = false;
+            if (key.Key == ConsoleKey.UpArrow && gameProcess.snakeDirection != "down") gameProcess.snakeDirection = "up";
+            if (key.Key == ConsoleKey.DownArrow && gameProcess.snakeDirection != "up") gameProcess.snakeDirection = "down";
+            if (key.Key == ConsoleKey.RightArrow && gameProcess.snakeDirection != "left") gameProcess.snakeDirection = "right";
+            if (key.Key == ConsoleKey.LeftArrow && gameProcess.snakeDirection != "right") gameProcess.snakeDirection = "left";
+        }
+    }
+}
---
+++ ZhenyaTask4/Program.cs
@@ -0,0 +1,123 @@
+﻿// Задача 4. (*) Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+// Решение не работает при m % 2 == 0 && n % 2 != 0 и работает до массива 8х8. Сложно
+
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MoveRight(int[,] array, int i, int j, int step)
+{
+    if (step == array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = step;
+        step++;
+        return step;
+    }
+    int currentWidth = array.GetLength(1) - j - 1;
+    for (int n = j; n < currentWidth; n++) // чтобы не сбить стартовую позицию
+    {
+        array[i, n] = step;
+        Console.WriteLine($"{i},{n}");
+        step++;
+    }
+    step = MoveDown(array, i, j, step);
+    return step;
+}
+
+int MoveDown(int[,] array, int i, int j, int step)
+{
+    int currentWidth = array.GetLength(1) - j - 1;
+    int currentHeight = array.GetLength(0) - i - 1;
+    for (int n = i; n < currentHeight; n++) // чтобы не сбить стартовую позицию
+    {
+        array[n, currentWidth] = step;
+        Console.WriteLine($"{n},{currentWidth}");
+        step++;
+    }
+    step = MoveLeft(array, i, j, step);
+    return step;
+}
+
+int MoveLeft(int[,] array, int i, int j, int step)
+{
+    int currentWidth = array.GetLength(1) - j - 1;
+    int currentHeight = array.GetLength(0) - i - 1;
+    for (int n = currentWidth; n > j; n--) // чтобы не сбить стартовую позицию
+    {
+        array[currentHeight, n] = step;
+        Console.WriteLine($"{currentHeight},{n}");
+        step++;
+    }
+    step = MoveUp(array, i, j, step);
+    return step;
+}
+
+int MoveUp(int[,] array, int i, int j, int step)
+{
+    int currentHeight = array.GetLength(0) - i - 1;
+    for (int n = currentHeight; n > i; n--) // чтобы не сбить стартовую позицию
+    {
+        array[n, j] = step;
+        Console.WriteLine($"{n},{j}");
+        step++;
+    }
+    return step;
+}
+
+void StepRoundArray(int[,] array, int i, int j, int step)
+{
+    // int k = i; //Start position
+    // int l = j; //Start position
+    // int number;
+    // if (step <= array.GetLength(0) / 2)
+    // {
+    //     if (step % 2 == 0 || array.GetLength(0) > 6) number = array[k + step, l + ((int)(Math.Round((double)step / 2)))] + 1; // Ужасная формула, понимаю, получил "тупым" подбором. Просто прибавлять step- не работает почему-то
+    //     else number = array[k + step, l] + 1;
+    //     MoveRight(array, k + step, l + step, number, step);
+    //     StepRoundArray(array, k, l, (step + 1));
+    // }
+    if (step <= array.GetLength(0) * array.GetLength(1))
+    {
+        Console.WriteLine();
+        step = MoveRight(array, i, j, step);
+        i++;
+        j++;
+        StepRoundArray(array, i, j, step);
+    }
+}
+
+void Main()
+{
+    int m = ReadData("Input an amount of the rows in array");
+    int n = ReadData("Input an amount of the columns in array");
+    int i = 0; // Start position index1
+    int j = 0; // Start position index2
+    int step = 1; // A necessary variable for recurtion
+    int[,] array = new int[m, n];
+    StepRoundArray(array, i, j, step);
+    PrintArray(array);
+}
+
+Main();
